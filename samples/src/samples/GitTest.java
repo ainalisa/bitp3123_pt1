181d04f56a5949f63283f6f1213bfd14cb6b8a38
ainalisa <ainakhaliesa@gmail.com>
2023-04-11 16:16:38 +0800

    Another commit to the repository

--- samples/src/samples/GitTest.java
+++ samples/src/samples/GitTest.java
@@ -1,8 +1,12 @@
 package samples;
 
+
+// This is an example of Git test program
+
 public class GitTest {
 	public static void main(String[] args) {
         System.out.println("Git Is Fun");
         System.out.println("Git is cool");
+        
     }
 }
